cc3ef0b5de70f92dd4e5159742c7ff129ce7c568
igor <igorhefner@gmail.com>
2025-06-05 08:34:30 +0200

    Dodana funkcija findById, te  create,update,remove u produkt service

--- src/product/products.service.ts
+++ src/product/products.service.ts
@@ -1,23 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { Product } from '@prisma/client';
+import { Prisma, Product } from '@prisma/client';
 
 @Injectable()
 export class ProductsService {
- async remove(id: number) {
-  return this.prisma.product.delete({
-    where: { id },
-  });
-}
   constructor(private prisma: PrismaService) {}
 
   async findAll(): Promise<Product[]> {
     return this.prisma.product.findMany();
   }
 
+  async findById(id: number): Promise<Product | null> {
+    return this.prisma.product.findUnique({
+      where: { id },
+    });
+  }
+
   async findOne(slug: string): Promise<Product | null> {
     return this.prisma.product.findUnique({
       where: { slug },
+    });
+  }
+
+  async create(data: {
+    name: string;
+    price: string;
+    image: string;
+    slug: string;
+    description?: string;
+    details?: string;
+  }) {
+    return this.prisma.product.create({
+      data,
+    });
+  }
+
+  async update(id: number, data: Partial<Prisma.ProductUpdateInput>) {
+    return this.prisma.product.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async remove(id: number) {
+    return this.prisma.product.delete({
+      where: { id },
     });
   }
 }
